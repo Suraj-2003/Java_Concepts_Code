de2420596eb9b072d569bcc4557e957269dca1a7
Suraj More <moresuraj9511@gmail.com>
2025-12-27 09:39:11 +0530

    Display Jay Ganesh

---
+++ Demo.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+// import is a keyword which is use to include external files
+// java is a super pacakge
+// lang is a sub pacakge
+// * is used to all elements in pacakges
+
+// class is a keyword to declare 
+// Demo is user defined class
+class Demo{
+
+    // Entry point function of our program
+    public static void main(String arg[])
+    {
+        System.out.println("Jay Ganesh...!"); // Jay Ganesh        
+    }
+}
+
+// Compiler command : Javac Demo.java
+// Interpreter Command : Java Demo
