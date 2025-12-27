8dca7a984975c9f4d18a52a0861872e34ce45e58
Suraj More <moresuraj9511@gmail.com>
2025-12-27 11:28:06 +0530

    final class in java

---
+++ final_class.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+
+/*
+    final class is such a class which can not be inherited.
+    We can't reuse that class but still we can create object of that class.
+*/
+
+final class Base    // final class
+{
+
+}
+
+class Derived extends Base
+{
+
+}
+
+class final_class
+{
+    public static void main(String arg[])
+    {
+
+    }
+}
