37a7e54c2cdcd9d9be878e4d8745ad7abb2bf5e5
Suraj More <moresuraj9511@gmail.com>
2025-12-31 09:43:59 +0530

    Multidimentional Array

---
+++ Array_Multi.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+// Multi dimentional Array
+class Array_Multi
+{
+    public static void main(String arg[])
+    {
+        // System.out.println("Jay Ganesh...!");
+        int i = 0, j = 0;
+        int Arr[][] = new int[2][3];
+
+        Arr[0][0] = 10;
+        Arr[0][1] = 20;
+        Arr[0][2] = 30;
+        Arr[1][0] = 40;
+        Arr[1][1] = 50;
+        Arr[1][2] = 60;
+        
+        for(i = 0; i < Arr.length; i++)
+        {
+            for(j = 0; j < Arr[i].length; j++)
+            {
+                System.out.println(Arr[i][j]);
+            }
+        }
+    }
+}
