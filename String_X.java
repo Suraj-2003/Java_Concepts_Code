f1b963ce0bfbaca041ca6bc9a47e5797bef317a4
Suraj <moresuraj9511@gmail.com>
2026-01-02 11:21:16 +0530

    Describe String in Java

---
+++ String_X.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+
+class String_X
+{
+    public static void main(String arg[])
+    {
+        // System.out.println("Jay Ganesh...!");
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = str1+str2;
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);   // String Concatination
+
+        String str4 = "Hello";
+
+        String str5 = new String("Hello");
+        String str6 = new String("Hello");
+        String str7 = new String("World");
+        
+    }
+}
